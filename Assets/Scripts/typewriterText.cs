86dc63ce9779c7743873515c96ce35cfc095efb9
squidgeface <alevjen@hotmail.com>
2020-08-19 10:59:58 +1200

    text mesh pro added

---
+++ Assets/Scripts/typewriterText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class typewriterText : MonoBehaviour
+{
+    public float delay = 0.1f;
+    public string fullText;
+    private string curText = "";
+    public bool startText = false;
+
+
+    private void Update()
+    {
+        if (startText)
+        {
+            StartCoroutine(RevealText());
+            startText = false;
+        }
+    }
+
+    private IEnumerator RevealText()
+    {
+        for (int i = 0; i < fullText.Length; i++)
+        {
+            curText = fullText.Substring(0, i);
+            this.GetComponent<TextMeshPro>().text = curText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
