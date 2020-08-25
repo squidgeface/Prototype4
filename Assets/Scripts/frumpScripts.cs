66f9781cf7529dc28488acf63c8fa80bd41d6bde
squidgeface <alevjen@hotmail.com>
2020-08-26 10:06:22 +1200

    script updates

---
+++ Assets/Scripts/frumpScripts.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class frumpScripts : MonoBehaviour
+{
+    public string Lvl1Option1 = "My people tell me coal is the problem, and my people are the best. " +
+        "So I said the only solution is to burn all the coal and get rid of it, And that was MY idea. " +
+        "My people were amazed. They hadn’t thought of that. They literally gasped at how good the idea was.";
+
+    public string Lvl1Option2 = "You know I did some research, very in-depth research and discovered that factories are the problem. " +
+        "I discovered this myself. So I’m shutting down our factories, I can do that you know. And I'll be sending our production to China. " +
+        "China is very advanced. A very advanced people. Some of the most advanced I've been told. Not only does this help the environment." +
+        "It also creates jobs now that people don’t have to work in those terrible factories.";
+
+    public string Lvl1Option3 = "People do not think “America the great” when they hear 2nd place, this is what i’ve always said. " +
+        "I promised to make USA number one, and that's what I plan to do. We are getting rid of those old fossil fuels, that's what fossil means. " +
+        "And will be focusing on renewable resources like coal. You know I made some coal in my fireplace yesterday, and the brilliant idea came to me. " +
+        "We just make more coal and use that power. This ensures we get number one in the carbon mission.";  
+
+    public string Lvl2Option1;
+    public string Lvl2Option2;
+    public string Lvl2Option3;
+    public string Lvl3Option1;
+    public string Lvl3Option2;
+    public string Lvl3Option3;
+
+}
--- Assets/Scripts/typewriterText.cs
+++ Assets/Scripts/typewriterText.cs
@@ -13,6 +13,9 @@ public class typewriterText : MonoBehaviour
     public bool clicked = false;
     public int playAudio = 0;
     public bool isPlaying = false;
+    public int level = 1;
+    public levelScript LevelManager;
+    public frumpScripts frumpText;
 
     public AudioSource trump1;
     public AudioSource trump2;
@@ -26,33 +29,95 @@ public class typewriterText : MonoBehaviour
             startText = false;
          
         }
-
-        if (playAudio == 1 && isPlaying)
+        if (level == 1)
         {
-            this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true;
-            trump1.Play();
-            fullText = "My people tell me coal is the issue, and my people are the best. So I said the only solution is to burn all the coal and get rid of it, And that WAS my idea. My people were amazed. They hadn’t thought of that. They literally gasped at how good the idea was.";
-            StartCoroutine(RevealText());
-            isPlaying = false;
-            clicked = false;
+            if (playAudio == 1 && isPlaying)
+            {
+                this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true;
+                trump1.Play();
+                fullText = frumpText.GetComponent<frumpScripts>().Lvl1Option1;
+                StartCoroutine(RevealText());
+                isPlaying = false;
+                clicked = false;
+            }
+            else if (playAudio == 2 && isPlaying)
+            {
+                this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true;
+                trump2.Play();
+                fullText = frumpText.GetComponent<frumpScripts>().Lvl1Option2;
+                StartCoroutine(RevealText());
+                isPlaying = false;
+                clicked = false;
+            }
+            else if (playAudio == 3 && isPlaying)
+            {
+                this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true;
+                trump3.Play();
+                fullText = frumpText.GetComponent<frumpScripts>().Lvl1Option3;
+                StartCoroutine(RevealText());
+                isPlaying = false;
+                clicked = false;
+            }
         }
-        else if (playAudio == 2 && isPlaying)
+        else if (level == 2)
         {
-            this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true;
-            trump2.Play();
-            fullText = "You know I did some research, very in-depth research and discovered that factories are the problem. I discovered this myself.So I’m shutting down our factories, I can do that you know.And I'll be sending our production to CHINA. China is very advanced. A very advanced people. Some of the most advanced I've been told.Not only does this help the environment.It also creates jobs now that people don’t have to work in those terrible factories.";
-            StartCoroutine(RevealText());
-            isPlaying = false;
-            clicked = false;
+            if (playAudio == 1 && isPlaying)
+            {
+                this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true;
+                trump1.Play();
+                fullText = frumpText.GetComponent<frumpScripts>().Lvl2Option1;
+                StartCoroutine(RevealText());
+                isPlaying = false;
+                clicked = false;
+            }
+            else if (playAudio == 2 && isPlaying)
+            {
+                this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true;
+                trump2.Play();
+                fullText = frumpText.GetComponent<frumpScripts>().Lvl2Option2;
+                StartCoroutine(RevealText());
+                isPlaying = false;
+                clicked = false;
+            }
+            else if (playAudio == 3 && isPlaying)
+            {
+                this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true;
+                trump3.Play();
+                fullText = frumpText.GetComponent<frumpScripts>().Lvl2Option3;
+                StartCoroutine(RevealText());
+                isPlaying = false;
+                clicked = false;
+            }
         }
-        if (playAudio == 3 && isPlaying)
+        else if (level == 2)
         {
-            this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true;
-            trump3.Play();
-            fullText = "People do not think “America the great” when they hear 2nd place, this is what i’ve always said. I promised to make USA number 1 and that's what I plan to do. We are getting rid of those old fossil fuels, that's what fossil means. And be focusing on renewable resources like coal. You know I made some coal in my fireplace the other day, and the brilliant idea came to me. We just make more coal and use that power. This ensures we get number one in the carbon mission.";
-            StartCoroutine(RevealText());
-            isPlaying = false;
-            clicked = false;
+            if (playAudio == 1 && isPlaying)
+            {
+                this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true;
+                trump1.Play();
+                fullText = frumpText.GetComponent<frumpScripts>().Lvl3Option1;
+                StartCoroutine(RevealText());
+                isPlaying = false;
+                clicked = false;
+            }
+            else if (playAudio == 2 && isPlaying)
+            {
+                this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true;
+                trump2.Play();
+                fullText = frumpText.GetComponent<frumpScripts>().Lvl3Option2;
+                StartCoroutine(RevealText());
+                isPlaying = false;
+                clicked = false;
+            }
+            else if (playAudio == 3 && isPlaying)
+            {
+                this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true;
+                trump3.Play();
+                fullText = frumpText.GetComponent<frumpScripts>().Lvl3Option3;
+                StartCoroutine(RevealText());
+                isPlaying = false;
+                clicked = false;
+            }
         }
     }
 
