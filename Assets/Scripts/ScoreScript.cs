7be354b139bea46a095a330f38246500b69003d4
RavenClancey <RavenClancey1@gmail.com>
2020-08-26 09:30:27 +1200

    started score tracked + menu takes you to first scene

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int GoodScore = 0;
+    public int StupidScore = 0;
+    public int EvilScore = 0;
+
+    void Start()
+    {
+        Object.DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoodEnding()
+    {
+        //goto good scene
+    }
+    public void StupidEnding()
+    {
+        //goto stupid scene
+    }
+    public void EvilEnding()
+    {
+        //goto Evil scene
+    }
+}
