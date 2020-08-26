adead767126ccf285a7ba6f7d4fe9277550f103a
squidgeface <alevjen@hotmail.com>
2020-08-27 11:40:27 +1200

    changes

--- Assets/Scripts/typewriterText.cs
+++ Assets/Scripts/typewriterText.cs
@@ -32,37 +32,42 @@ public class typewriterText : MonoBehaviour
 
     private void Update()
     {
-       
-        timer += Time.deltaTime;
-        
-        if (timer > 5 && done == false)
+        if (level < 6)
         {
-            LevelManager.GetComponent<levelScript>().Clicked = true;
-            Reporter.Play();
-            done = true;
-            timer = 0;
+            timer += Time.deltaTime;
+
+            if (timer > 5 && done == false)
+            {
+                LevelManager.GetComponent<levelScript>().Clicked = true;
+                Reporter.Play();
+                done = true;
+                timer = 0;
+            }
+
+            if (done && timer > 7)
+            {
+                FindObjectOfType<CameraMover>().activated = true;
+                gameObject.GetComponentInChildren<SpriteRenderer>().enabled = false;
+                this.GetComponent<TextMeshPro>().text = "";
+                umm.Play();
+                timer = 0;
+            }
+
+            level = LevelManager.GetComponent<levelScript>().level;
+            startText = LevelManager.GetComponent<levelScript>().Clicked;
+
+            if (startText && isPlaying)
+            {
+                gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true;
+                StartCoroutine(RevealText());
+
+                startText = false;
+                isPlaying = false;
+            }
         }
-
-        if (done && timer > 7)
+        else
         {
-            FindObjectOfType<CameraMover>().activated = true;
-            gameObject.GetComponentInChildren<SpriteRenderer>().enabled = false;
-            this.GetComponent<TextMeshPro>().text = "";
-            umm.Play();
-            timer = 0;
-        }
-
-       level = LevelManager.GetComponent<levelScript>().level;
-       startText = LevelManager.GetComponent<levelScript>().Clicked;
-
-        if (startText && isPlaying)
-        {
-            gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true;
-            gameObject.GetComponentInChildren<BoxCollider>().enabled = true;
             StartCoroutine(RevealText());
-          
-            startText = false;
-            isPlaying = false;
         }
         
     }
