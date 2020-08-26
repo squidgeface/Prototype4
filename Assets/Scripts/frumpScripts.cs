90079e89e1f4a996a5823137b448defea5a3c6bd
squidgeface <alevjen@hotmail.com>
2020-08-27 11:34:25 +1200

    Update frumpScripts.cs

--- Assets/Scripts/frumpScripts.cs
+++ Assets/Scripts/frumpScripts.cs
@@ -6,35 +6,35 @@ public class frumpScripts : MonoBehaviour
 {
     public string Lvl1Option1 = "My people tell me coal is the problem, and my people are the best. " +
         "So I said the only solution is to burn all the coal and get rid of it, And that was my idea. " +
-        "My people were amazed. They hadn’t thought of that. They literally gasped at how good the idea was.";
+        "My people were amazed. They hadn’t thought of that. They literally gasped at how good the idea was. ";
 
     public string Lvl1Option2 = "You know I did some research, very in-depth research and discovered that factories are the problem. " +
         "I discovered this myself. So I’m shutting down our factories, I can do that you know. And I'll be sending our production to China. " +
         "China is very advanced. A very advanced people. Some of the most advanced I've been told. Not only does this help the environment." +
-        "It also creates jobs now that people don’t have to work in those terrible factories.";
+        "It also creates jobs now that people don’t have to work in those terrible factories. ";
 
     public string Lvl1Option3 = "People do not think “America the great” when they hear 2nd place, this is what i’ve always said. " +
         "I promised to make USA number one, and that's what I plan to do. We are getting rid of those old fossil fuels, that's what fossil means. " +
         "And we will be focusing on renewable resources like coal. You know I made some coal in my fireplace yesterday, and the brilliant idea came to me. " +
-        "We just make more coal and use that power. This ensures we get number one in the carbon mission.";  
+        "We just make more coal and use that power. This ensures we get number one in the carbon mission. ";  
 
     public string Lvl2Option1 = "It’s going to be, really, a voluntary thing. You can do it. You don’t have to do it. " +
         "I’m choosing not to do it, but some people may want to do it, and that’s OK. It may be good." +
-        "Probably will. They’re making a recommendation. It’s only a recommendation. Thanks for the recommendation though.";
+        "Probably will. They’re making a recommendation. It’s only a recommendation. Thanks for the recommendation though. ";
 
     public string Lvl2Option2 = "Ive been told masks help, they help a lot. I checked with my people and they confirm what I said. " +
         "Personally I will be using my Batman mask. My people tell me any mask is better than none so if spider-man is all you’ve got, go with that. " +
-        "My people also tell me tracing is important. Myself i’m more of a free hand artist, but this is what my people say.";
+        "My people also tell me tracing is important. Myself i’m more of a free hand artist, but this is what my people say. ";
 
     public string Lvl2Option3 = "Suppose that we hit the body with a tremendous force, whether it’s ultraviolet or just very powerful light. " +
         "Supposing you brought the light inside the body, which you can do either through the skin or in some other way. " +
         "Is there a way we can do something like that by injection inside or almost a cleaning? " +
         "It sounds interesting to me, so we’ll see. But the whole concept of the light, the way it kills it in one minute. " +
-        "That’s pretty powerful, let’s fund that.";
+        "That’s pretty powerful, let’s fund that. ";
 
     public string Lvl3Option1 = "I've always said, if a business is struggling you should let it die. " +
         "The USPS says it  will cause issues with the votes if I dont give them money. They are threatening me. " +
-        "I could shut them down if I wanted to, I really could. I have that power. Maybe I'll just add huge taxes, who knows I haven't decided.";
+        "I could shut them down if I wanted to, I really could. I have that power. Maybe I'll just add huge taxes, who knows I haven't decided. ";
 
     public string Lvl3Option2 = "People tell me they are worried that Russia may try and interfere with the elections, but there is no need to panic. " +
         "I asked Putin myself, a good friend of mine. And he said “No”. And that's good enough for me. He is a very trustable man. ";
