58980e1fc0474b359197eaaa16e53e289d9cc906
squidgeface <alevjen@hotmail.com>
2020-08-26 14:55:00 +1200

    scene changes

---
+++ Assets/fadeScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fadeScript : MonoBehaviour
+{
+    public float speed = 255.0f;
+    Color tempMat;
+
+    private void Start()
+    {
+        tempMat = gameObject.GetComponent<MeshRenderer>().material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed -= 0.001f;
+        tempMat.a = speed;
+        gameObject.GetComponent<MeshRenderer>().material.color = tempMat;
+    }
+}
