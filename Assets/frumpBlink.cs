b383319c8194b2fe35e31df4390bcd48129cc970
squidgeface <alevjen@hotmail.com>
2020-08-27 10:08:51 +1200

    updates

---
+++ Assets/frumpBlink.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class frumpBlink : MonoBehaviour
+{
+    public Camera camera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (camera.transform.position.z < -10.0f)
+        {
+            gameObject.GetComponent<Renderer>().materials[0].CopyPropertiesFromMaterial(gameObject.GetComponent<Renderer>().materials[1]);
+        }
+    }
+}
